1f2645ce6e4f2d8eea0aeddd8568e9f3752d5c6c
Strallia Chao <134454347+strallia@users.noreply.github.com>
2024-02-01 22:23:12 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Project
+
+# Purpose
+
+# Template Features
+
+- npm & configs
+- Webpack & configs
+- ESLint
+- Prettier
+- Babel
+
+# Customize Template
+
+## package.json
+
+- "name" property
+
+## wepack.config.js
+
+- favicon path
+
+## .browserlistrc
+
+- configure which browsers babel should transpile for
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    static: './src',
+  },
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      favicon: '',
+    }),
+  ],
+};
