bf11755edcdd7631cbc879167058a3869b130f51
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-02-02 19:46:29 -0800

    Style city title and menu

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
         type: 'asset/resource',
       },
       {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
