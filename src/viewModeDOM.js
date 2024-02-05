de4afccc4988eddd24fe53893c2410537ccfb2c4
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-02-04 21:30:09 -0800

    Simplify code for toggling view mode

--- src/viewModeDOM.js
+++ src/viewModeDOM.js
@@ -3,34 +3,23 @@ const menuContainer = document.querySelector('.menu-container');
 const contentContainer = document.querySelector('.content-container');
 const dayFrames = document.querySelectorAll('.day-frame');
 
-window.addEventListener('resize', () => {
+const toggleViewMode = () => {
   if (window.innerWidth < 920) {
-    body.classList.remove('full-view');
-    body.classList.add('narrow-view');
-
-    menuContainer.classList.remove('full-view');
-    menuContainer.classList.add('narrow-view');
-
-    contentContainer.classList.remove('full-view');
-    contentContainer.classList.add('narrow-view');
-
-    dayFrames.forEach((frame) => {
-      frame.classList.remove('full-view');
-      frame.classList.add('narrow-view');
-    });
+    [body, menuContainer, contentContainer, ...dayFrames].forEach(
+      (container) => {
+        container.classList.remove('full-view');
+        container.classList.add('narrow-view');
+      },
+    );
   } else {
-    body.classList.add('full-view');
-    body.classList.remove('narrow-view');
-
-    menuContainer.classList.add('full-view');
-    menuContainer.classList.remove('narrow-view');
-
-    contentContainer.classList.add('full-view');
-    contentContainer.classList.remove('narrow-view');
-
-    dayFrames.forEach((frame) => {
-      frame.classList.add('full-view');
-      frame.classList.remove('narrow-view');
-    });
+    [body, menuContainer, contentContainer, ...dayFrames].forEach(
+      (container) => {
+        container.classList.add('full-view');
+        container.classList.remove('narrow-view');
+      },
+    );
   }
-});
+};
+toggleViewMode();
+
+window.addEventListener('resize', toggleViewMode);
